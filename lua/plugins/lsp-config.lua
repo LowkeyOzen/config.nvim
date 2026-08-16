f07a7dd1194dd5525c873f1fe17e20de315a4fba
LowkeyOzen <265652529+LowkeyOzen@users.noreply.github.com>
2026-08-16 01:29:41 +0100

    fix odin and lua lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,8 +1,22 @@
 return{
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+       {
+          "folke/lazydev.nvim",
+          ft = "lua", -- only load on lua files
+          opts = {
+            library = {
+              -- See the configuration section for more details
+              -- Load luvit types when the `vim.uv` word is found
+              { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+          },
+        },
+    },
     config = function()
-        vim.lsp.enable("ols")
+        vim.lsp.enable("ols") -- dont forget to add ols
+        vim.lsp.enable("lua_ls") -- same with lua_ls
     end,
   }
 
